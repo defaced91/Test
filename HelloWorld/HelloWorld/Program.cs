32f45d28949a9357b718c6371e944e4aa22b188a
Dennis Woodside <dennis.woodside@arcor.de>
2016-06-10 12:24:58 +0200

    .gitignore test

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hallo 10FI5H");
+            Console.WriteLine("Bitte druecken Sie eine beliebige Taste um das Programm zu beenden.");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
